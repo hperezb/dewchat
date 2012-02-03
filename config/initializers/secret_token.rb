ea9525aa4ea695211e8f851ce1e90e8d86743a27
unknown <hperez@lp-gaf02.cepersa.com.pe>
2012-02-02 20:03:45 -0500

    frist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DewChat::Application.config.secret_token = 'dda61ce21eef072cc885704b3655e6f3e08c3335e6667ed44d66130c1904400c04cc4192b908dcd0d0025d69f428e2b4ae35b48239e5f00585dd7fa70fc5aa13'
